805a7f6e36ef77317e64d91c2a2145f44d1e2f73
Alex Cave <alex.cave@outlook.com.au>
2025-08-02 12:09:35 +1000

    About to make major changes.

--- makeboot.sh
+++ makeboot.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-west build -p -b xiao_ble --no-sysbuild -s $PWD/../bootloader/mcuboot/boot/zephyr -- -DDTC_OVERLAY_FILE=$PWD/boards/xiao_ble.overlay -DCONF_FILE=$PWD/boot.conf
+west build -p -b xiao_ble/nf52840/sense --no-sysbuild -s $PWD/../bootloader/mcuboot/boot/zephyr -- -DDTC_OVERLAY_FILE=$PWD/boards/xiao_ble.overlay -DCONF_FILE=$PWD/boot.conf
--- menu.sh
+++ menu.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-west build -b xiao_ble $PWD/../bootloader/mcuboot/boot/zephyr -t menuconfig -- -DDTC_OVERLAY_FILE=$PWD/boards/xiao_ble.overlay -DCONF_FILE=$PWD/boot.conf 
+west build -b xiao_ble --no-sysbuild $PWD/../bootloader/mcuboot/boot/zephyr -t menuconfig -- -DDTC_OVERLAY_FILE=$PWD/boards/xiao_ble.overlay -DCONF_FILE=$PWD/boot.conf 
